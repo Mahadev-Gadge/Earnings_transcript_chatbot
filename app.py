88729fc285c5381f333724a79455171728ed3a60
Mahadev-Gadge <119348123+Mahadev-Gadge@users.noreply.github.com>
2023-12-05 01:16:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,9 @@ def earnings_transcript_assistant():
                                  "Who is the CEO of company name mentioned in the transcript?")   
                                 )
             if content is None:
-                   content=st.text_input("Ask you question ", "Which company transcript is this belongs to")
+                   content = st.text_input("Ask you question ", "Which company transcript is this belongs to")
+            else:
+                   content = content
             message = st.session_state.client.beta.threads.messages.create(thread_id=st.session_state.thread.id, role="user", content=content)
             run = st.session_state.client.beta.threads.runs.create(thread_id=st.session_state.thread.id, assistant_id=st.session_state.assistant.id)
             # Poll for the run to complete and retrieve the assistant's messages
